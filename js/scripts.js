ff2c9cc4e56712b50bbec1b3ca4525a6d45b6140
Daren Schaad and Jordan Meier <darenschaad@gmail.com>
2016-01-12 15:50:40 -0800

    Add a JS folder/scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("button#dark").click(function() {
+    $("body").addClass("darkBackground");
+  });
+
+  $("button#light").click(function() {
+    $("body").addClass("lightBackground");
+  });
+
+});
