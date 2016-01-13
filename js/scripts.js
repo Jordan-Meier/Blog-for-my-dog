1ed03fe6d8ba95702329baa275c9dc3b5d420e0d
Daren Schaad and Jordan Meier <darenschaad@gmail.com>
2016-01-12 16:03:04 -0800

    import bootstrap and jQuery files

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   $("button#dark").click(function() {
+    $("body").removeClass();
     $("body").addClass("darkBackground");
   });
 
   $("button#light").click(function() {
+    $("body").removeClass();
     $("body").addClass("lightBackground");
   });
 
